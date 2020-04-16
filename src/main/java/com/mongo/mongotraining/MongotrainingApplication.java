5f75fc35a2213cb1afb5cb6fd2ec6579f8e6bfd0
Helder Falcão <helder@ciandt.com>
2020-04-16 14:21:40 -0300

    Initial project structure

---
+++ src/main/java/com/mongo/mongotraining/MongotrainingApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.mongotraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongotrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongotrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mongo/mongotraining/MongotrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongo.mongotraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongotrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
